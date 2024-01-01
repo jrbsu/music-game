b7cd95e378e91827719ce53a6a47ebf166802340
jrbsu <jrbsutherland@gmail.com>
2024-01-01 22:14:34 +0000

    Co-authored-by: Adam Sutherland <adsuth@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const MAX_ATTEMPTS = 5;
-let song_array = [];
 let points = MAX_ATTEMPTS;
 let thisYear = 0;
 let total = 0;
-let game_over_text = "Game over YEAR!"
+let game_over_text = "Bad luck!"
 
+let all_songs   = []
+let songs       = []
 
 const MIN_YEAR = 1953
 const MAX_YEAR = 2023
@@ -14,9 +15,14 @@ const guessInput     = document.getElementById( "guess" )
 const guessButton    = document.getElementById( "guessButton" )
 const giveUpButton   = document.getElementById( "giveUpButton" )
 const nextSongButton = document.getElementById( "nextSongButton" )
+const newGameButton  = document.getElementById( "newGameButton" )
+const songItems      = document.getElementById( "songs" )
+
+const endGameInput   = document.getElementById( "endGameInput" )
+const inGameInput    = document.getElementById( "inGameInput" )
 
 function randomYear() {
-    let random = Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR) + MIN_YEAR);
+    let random = Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR) + MIN_YEAR)
     return random
 }
 
@@ -28,45 +34,70 @@ function shuffleArray(array) {
     return array;
 }
 
-fetch('./tracks.json')
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        song_array = [];
-        thisYear = randomYear();
-        json.forEach(function(element) {
-            let year = element["year"]
-            let title = element["title"]
-            let artist = element["artist"]
-            if (thisYear == year) {
-                song_array.push([title, artist])
-            }
-        });
-        shuffleArray(song_array);
-        document.getElementById('songs').innerHTML = '<div class="song"><div class="title">' + song_array[0][0] + '</div>\n<div class="artist">' + song_array[0][1] + '</div></div>';
-        console.log( {correctYear: thisYear});
-        updatePointsBox()
+async function fetchTracks() {
+    if ( songs.length > 0 ) return
+
+    const res = await fetch('./tracks.json')
+    const json = await res.json()
+
+    all_songs = [ ...json ]
+}
+
+async function nextRound() {
+    await fetchTracks()
+
+    thisYear = randomYear();
+    songs = []
+    game_over_text = "Bad luck! The year was " + thisYear
+    all_songs.forEach( element => {
+        const year = element["year"]
+        const title = element["title"]
+        const artist = element["artist"]
+
+        if ( thisYear === year )
+            songs.push([title, artist])
     });
 
-document.getElementById( "guess" ).addEventListener( "input", e => {
+    shuffleArray( songs )
+    document.getElementById('songs').innerHTML = '<div class="song song-5"><div class="title">' + songs[0][0] + '</div>\n<div class="artist">' + songs[0][1] + '</div></div>'
+    console.log( {correctYear: thisYear})
+    updatePointsBox()
+    startGame()
+}
+
+guessInput.addEventListener( "input", e => {
     e.target.value = e.target.value.replace( /[^0-9]/g, "" )
 } )
 
-nextSongButton.addEventListener('click', newSong);
+nextSongButton.addEventListener( "click", handleIncorrect );
 
 function newSong() {
-    if ( points === 0 ) return
+    guessInput.focus()
+    removeSongItemClasses()
 
     if ( points > 1 )
-    {
-        const newSong = document.createElement('div');
-        newSong.className = "song";
-        newSong.innerHTML = '<div class="title">' + song_array[points][0] + '</div>\n<div class="artist">' + song_array[points][1] + '</div>';
-        document.getElementById('songs').appendChild(newSong);
-    }
+        songItems.prepend( createNewSongItem() );
+
     points--
     updatePointsBox()
+
+    if ( points === 0 ) handleGameOver()
+}
+
+function createNewSongItem() {
+    const songElement = document.createElement( "div" )
+    songElement.className = "song song-" + (points - 1)
+    songElement.innerHTML = `<div class="title">${ songs[points][0] }</div><div class="artist">${ songs[points][1] }</div>`
+
+    return songElement
+}
+
+function removeSongItemClasses() {
+    let songNodes = document.getElementsByClassName("song");
+    for ( const songNode of songNodes ) {
+        removeClassByPrefix(songNode, "song-");
+    }
+    
 }
 
 function removeClassByPrefix(node, prefix) {
@@ -77,22 +108,27 @@ function removeClassByPrefix(node, prefix) {
 
 function handleGameOver() {
     pointsElement.innerHTML = game_over_text;
+    total = 0;
+    removeSongItemClasses()
     setDisableAllElements( true )
+    showEndGameInput()
 }
 
 function handleSuccess() {
     var pointsNode = document.getElementById("points");
     removeClassByPrefix(pointsNode, "points-");
     document.getElementById("points").classList.add("rainbowGradient");
-    pointsElement.innerHTML = `You win ${points} points!!`
     total += points;
+    pointsElement.innerHTML = `${thisYear} is right! You win ${points} point${points === 1 ? "" : "s"}!!<br />Now you have ${total} point${total === 1 ? "" : "s"}.`
     setDisableAllElements( true )
+    showEndGameInput()
 }
 
 function handleIncorrect() {
     if ( points > 0 ) return newSong();
     updatePointsBox();
     handleGameOver();
+    updateBodyBackground( points )
 }
 
 function setDisableAllElements( disabled = true ) {
@@ -105,16 +141,19 @@ function setDisableAllElements( disabled = true ) {
 function updatePointsBox() {
     const pointsClass = "points-" + points;
 
-    pointsElement.classList.remove( "points-" + (points + 1) );
-    pointsElement.classList.add( pointsClass );
+    addPointsClassToAlert( points )
 
     if ( points !== 0 ) {
-        pointsElement.innerHTML = `For ${points} points...`
+        pointsElement.innerHTML = `For ${points} point${points === 1 ? "" : "s"}...`
     }
     else {
         pointsElement.innerHTML = game_over_text;
     }
     
+}
+
+function updateBodyBackground() {
+    document.body.className = `body-${points}`
 }
 
 function wasGuessValid( guess ) {
@@ -124,20 +163,65 @@ function wasGuessValid( guess ) {
     )
 }
 
+function displayModal( guess, year ) {
+    let gap = year - guess;
+    let modal = document.getElementById("modal");
+    let modalText = document.getElementById("modal-text");
+    if (gap < 0) {
+        gap = gap * -1;
+    }
+    if (gap < 3) {
+        modalText.innerHTML = "So close! 🔥";
+    } else if (gap < 10) {
+        modalText.innerHTML = "Not bad! 🤷‍♂️";
+    } else {
+        modalText.innerHTML = "Way off! ❄️";
+    }
+
+    modal.classList.add( "showModal" );
+    setTimeout( () => { modal.classList.remove( "showModal" ) }, 800 )
+}
+
 function handleGuess() {
     if ( guessButton.disabled ) return
 
     const guess = +document.getElementById("guess").value
+    guessInput.focus()
     
     if ( !wasGuessValid( guess ) ) return
 
-    if ( guess === thisYear )
+    if ( guess === thisYear ) {
         handleSuccess();
-    else
+    }
+    else {
+        displayModal(guess, thisYear);
         handleIncorrect();
+    }
 
     guessInput.value = ""
 
+}
+
+function showEndGameInput() {
+    endGameInput.classList.remove( "hidden" )
+    inGameInput.classList.add( "hidden" )
+}
+function showInGameInput() {
+    inGameInput.classList.remove( "hidden" )
+    endGameInput.classList.add( "hidden" )
+}
+
+function startGame() {
+    points   = MAX_ATTEMPTS
+    showInGameInput()
+    setDisableAllElements( false )
+    updatePointsBox()
+    updateBodyBackground()
+}
+
+function addPointsClassToAlert( points ) {
+    const className = `points-${points}` 
+    pointsElement.className = `alert ${className}`
 }
 
 guessButton.addEventListener( "click", handleGuess )
@@ -148,3 +232,8 @@ giveUpButton.addEventListener( "click", () => {
 guessInput.addEventListener( "keydown", e => {
     if ( e.code === "Enter" ) handleGuess()
 } )
+
+newGameButton.addEventListener( "click", nextRound )
+
+// entrypoint
+nextRound() 
--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import json
+from youtubesearchpython import VideosSearch
 
 with open("data.tsv", "r") as f:
     data = f.read()
@@ -8,10 +9,13 @@ years = []
 
 for line in lines:
     song = line.split('\t')
-    print(song)
     year = int(song[3])
-    track = {"title":song[2], "artist":song[1], "year":year}
+    search_query = song[2] + " " + song[1]
+    videosSearch = VideosSearch(search_query, limit = 1)
+    video_id = str(videosSearch.result()["result"][0]["id"])
+    track = {"title":song[2], "artist":song[1], "year":year, "video_id":video_id}
     years.append(track)
+    print(track)
 
 json_object = json.dumps(years, indent = 4) 
 print(json_object)
