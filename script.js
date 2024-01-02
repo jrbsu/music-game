c2b890e493141a41f15ce6e440d94ee06c8d5182
Foks <jsutherland@wikimedia.org>
2024-01-02 18:50:32 +0000

    small tweaks, gradient background :O

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let thisYear = 0;
 let total = 0;
 let game_over_text = "Bad luck!"
 
-let all_songs   = []
-let songs       = []
+let all_songs = []
+let songs     = []
 
 const MIN_YEAR = 1953
 const MAX_YEAR = 2023
@@ -22,82 +22,82 @@ const endGameInput   = document.getElementById( "endGameInput" )
 const inGameInput    = document.getElementById( "inGameInput" )
 
 function randomYear() {
-    let random = Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR) + MIN_YEAR)
-    return random
+  let random = Math.floor(Math.random() * (MAX_YEAR - MIN_YEAR) + MIN_YEAR)
+  return random
 }
 
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 async function fetchTracks() {
-    if ( songs.length > 0 ) return
+  if ( songs.length > 0 ) return
 
-    const res = await fetch('./tracks.json')
-    const json = await res.json()
+  const res = await fetch('./tracks.json')
+  const json = await res.json()
 
-    all_songs = [ ...json ]
+  all_songs = [ ...json ]
 }
 
 async function nextRound() {
-    await fetchTracks()
+  await fetchTracks()
 
-    thisYear = randomYear();
-    songs = []
-    game_over_text = "Bad luck! The year was " + thisYear
-    all_songs.forEach( element => {
-        const year = element["year"]
-        const title = element["title"]
-        const artist = element["artist"]
+  thisYear = randomYear();
+  songs = []
+  game_over_text = "Bad luck! The year was " + thisYear
+  all_songs.forEach( element => {
+    const year = element["year"]
+    const title = element["title"]
+    const artist = element["artist"]
 
-        if ( thisYear === year )
-            songs.push([title, artist])
-    });
+    if ( thisYear === year )
+      songs.push([title, artist])
+  });
 
-    shuffleArray( songs )
-    document.getElementById('songs').innerHTML = '<div class="song song-5"><div class="title">' + songs[0][0] + '</div>\n<div class="artist">' + songs[0][1] + '</div></div>'
-    console.log( {correctYear: thisYear})
-    updatePointsBox()
-    startGame()
+  shuffleArray( songs )
+  document.getElementById('songs').innerHTML = '<div class="song song-5"><div class="title">' + songs[0][0] + '</div>\n<div class="artist">' + songs[0][1] + '</div></div>'
+  console.log( {correctYear: thisYear})
+  updatePointsBox()
+  startGame()
 }
 
 guessInput.addEventListener( "input", e => {
-    e.target.value = e.target.value.replace( /[^0-9]/g, "" )
+  e.target.value = e.target.value.replace( /[^0-9]/g, "" )
 } )
 
 nextSongButton.addEventListener( "click", handleIncorrect );
 
 function newSong() {
-    guessInput.focus()
-    removeSongItemClasses()
+  guessInput.focus()
+  removeSongItemClasses()
 
-    if ( points > 1 )
-        songItems.prepend( createNewSongItem() );
+  if ( points > 1 )
+    songItems.prepend( createNewSongItem() );
 
-    points--
-    updatePointsBox()
+  points--
+  updatePointsBox()
 
-    if ( points === 0 ) handleGameOver()
+  if ( points === 0 ) handleGameOver()
 }
 
 function createNewSongItem() {
-    const songElement = document.createElement( "div" )
-    songElement.className = "song song-" + (points - 1)
-    songElement.innerHTML = `<div class="title">${ songs[points][0] }</div><div class="artist">${ songs[points][1] }</div>`
+  const songElement = document.createElement( "div" )
+  songElement.className = "song song-" + (points - 1)
+  songElement.innerHTML = `<div class="title">${ songs[points][0] }</div><div class="artist">${ songs[points][1] }</div>`
 
-    return songElement
+  return songElement
 }
 
 function removeSongItemClasses() {
-    let songNodes = document.getElementsByClassName("song");
-    for ( const songNode of songNodes ) {
-        removeClassByPrefix(songNode, "song-");
-    }
-    
+  let songNodes = document.getElementsByClassName("song");
+  for ( const songNode of songNodes ) {
+    removeClassByPrefix(songNode, "song-");
+  }
+  
 }
 
 function removeClassByPrefix(node, prefix) {
@@ -107,130 +107,130 @@ function removeClassByPrefix(node, prefix) {
 }
 
 function handleGameOver() {
-    pointsElement.innerHTML = game_over_text;
-    total = 0;
-    removeSongItemClasses()
-    setDisableAllElements( true )
-    showEndGameInput()
+  pointsElement.innerHTML = game_over_text;
+  total = 0;
+  removeSongItemClasses()
+  setDisableAllElements( true )
+  showEndGameInput()
 }
 
 function handleSuccess() {
-    var pointsNode = document.getElementById("points");
-    removeClassByPrefix(pointsNode, "points-");
-    document.getElementById("points").classList.add("rainbowGradient");
-    total += points;
-    pointsElement.innerHTML = `${thisYear} is right! You win ${points} point${points === 1 ? "" : "s"}!!<br />Now you have ${total} point${total === 1 ? "" : "s"}.`
-    setDisableAllElements( true )
-    showEndGameInput()
+  var pointsNode = document.getElementById("points");
+  removeClassByPrefix(pointsNode, "points-");
+  document.getElementById("points").classList.add("rainbowGradient");
+  total += points;
+  pointsElement.innerHTML = `${thisYear} is right! You win ${points} point${points === 1 ? "" : "s"}!!<br />Now you have ${total} point${total === 1 ? "" : "s"}.`
+  setDisableAllElements( true )
+  showEndGameInput()
 }
 
 function handleIncorrect() {
-    if ( points > 0 ) return newSong();
-    updatePointsBox();
-    handleGameOver();
-    updateBodyBackground( points )
+  if ( points > 0 ) return newSong();
+  updatePointsBox();
+  handleGameOver();
+  updateBodyBackground( points )
 }
 
 function setDisableAllElements( disabled = true ) {
-    guessButton.disabled    = disabled
-    guessInput.disabled     = disabled
-    giveUpButton.disabled   = disabled
-    nextSongButton.disabled = disabled
+  guessButton.disabled  = disabled
+  guessInput.disabled   = disabled
+  giveUpButton.disabled   = disabled
+  nextSongButton.disabled = disabled
 }
 
 function updatePointsBox() {
-    const pointsClass = "points-" + points;
+  const pointsClass = "points-" + points;
 
-    addPointsClassToAlert( points )
+  addPointsClassToAlert( points )
 
-    if ( points !== 0 ) {
-        pointsElement.innerHTML = `For ${points} point${points === 1 ? "" : "s"}...`
-    }
-    else {
-        pointsElement.innerHTML = game_over_text;
-    }
-    
+  if ( points !== 0 ) {
+    pointsElement.innerHTML = `For ${points} point${points === 1 ? "" : "s"}...`
+  }
+  else {
+    pointsElement.innerHTML = game_over_text;
+  }
+  
 }
 
 function updateBodyBackground() {
-    document.body.className = `body-${points}`
+  document.body.className = `body-${points}`
 }
 
 function wasGuessValid( guess ) {
-    return (
-        guess >= MIN_YEAR &&
-        guess <= MAX_YEAR
-    )
+  return (
+    guess >= MIN_YEAR &&
+    guess <= MAX_YEAR
+  )
 }
 
 function displayModal( guess, year ) {
-    let gap = year - guess;
-    let modal = document.getElementById("modal");
-    let modalText = document.getElementById("modal-text");
-    if (gap < 0) {
-        gap = gap * -1;
-    }
-    if (gap < 3) {
-        modalText.innerHTML = "So close! 🔥";
-    } else if (gap < 10) {
-        modalText.innerHTML = "Not bad! 🤷‍♂️";
-    } else {
-        modalText.innerHTML = "Way off! ❄️";
-    }
+  let gap = year - guess;
+  let modal = document.getElementById("modal");
+  let modalText = document.getElementById("modal-text");
+  if (gap < 0) {
+    gap = gap * -1;
+  }
+  if (gap < 3) {
+    modalText.innerHTML = "So close! 🔥";
+  } else if (gap < 10) {
+    modalText.innerHTML = "Not bad! 🤷‍♂️";
+  } else {
+    modalText.innerHTML = "Way off! ❄️";
+  }
 
-    modal.classList.add( "showModal" );
-    setTimeout( () => { modal.classList.remove( "showModal" ) }, 800 )
+  modal.classList.add( "showModal" );
+  setTimeout( () => { modal.classList.remove( "showModal" ) }, 800 )
 }
 
 function handleGuess() {
-    if ( guessButton.disabled ) return
+  if ( guessButton.disabled ) return
 
-    const guess = +document.getElementById("guess").value
-    guessInput.focus()
-    
-    if ( !wasGuessValid( guess ) ) return
+  const guess = +document.getElementById("guess").value
+  guessInput.focus()
+  
+  if ( !wasGuessValid( guess ) ) return
 
-    if ( guess === thisYear ) {
-        handleSuccess();
-    }
-    else {
-        displayModal(guess, thisYear);
-        handleIncorrect();
-    }
+  if ( guess === thisYear ) {
+    handleSuccess();
+  }
+  else {
+    displayModal(guess, thisYear);
+    handleIncorrect();
+  }
 
-    guessInput.value = ""
+  guessInput.value = ""
 
 }
 
 function showEndGameInput() {
-    endGameInput.classList.remove( "hidden" )
-    inGameInput.classList.add( "hidden" )
+  endGameInput.classList.remove( "hidden" )
+  inGameInput.classList.add( "hidden" )
 }
 function showInGameInput() {
-    inGameInput.classList.remove( "hidden" )
-    endGameInput.classList.add( "hidden" )
+  inGameInput.classList.remove( "hidden" )
+  endGameInput.classList.add( "hidden" )
 }
 
 function startGame() {
-    points   = MAX_ATTEMPTS
-    showInGameInput()
-    setDisableAllElements( false )
-    updatePointsBox()
-    updateBodyBackground()
+  points   = MAX_ATTEMPTS
+  showInGameInput()
+  setDisableAllElements( false )
+  updatePointsBox()
+  updateBodyBackground()
 }
 
 function addPointsClassToAlert( points ) {
-    const className = `points-${points}` 
-    pointsElement.className = `alert ${className}`
+  const className = `points-${points}` 
+  pointsElement.className = `alert ${className}`
 }
 
 guessButton.addEventListener( "click", handleGuess )
 giveUpButton.addEventListener( "click", () => {
-    points = 0
-    handleIncorrect()
+  points = 0
+  handleIncorrect()
 } )
 guessInput.addEventListener( "keydown", e => {
-    if ( e.code === "Enter" ) handleGuess()
+  if ( e.code === "Enter" ) handleGuess()
 } )
 
 newGameButton.addEventListener( "click", nextRound )
