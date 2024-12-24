745a0b5e5682bddd3acb99e3d998b5fafdddd512
jrbsu <jrbsutherland@gmail.com>
2024-12-24 16:52:21 +0000

    actually update 2024 this time

--- create_json.py
+++ create_json.py
@@ -1,5 +1,5 @@
 import json
-from youtubesearchpython import VideosSearch
+# from youtubesearchpython import VideosSearch
 
 with open("data.tsv", "r") as f:
     data = f.read()
@@ -11,9 +11,10 @@ for line in lines:
     song = line.split('\t')
     year = int(song[3])
     search_query = song[2] + " " + song[1]
-    videosSearch = VideosSearch(search_query, limit = 1)
-    video_id = str(videosSearch.result()["result"][0]["id"])
-    track = {"title":song[2], "artist":song[1], "year":year, "video_id":video_id}
+    # videosSearch = VideosSearch(search_query, limit = 1)
+    # video_id = str(videosSearch.result()["result"][0]["id"])
+    # track = {"title":song[2], "artist":song[1], "year":year, "video_id":video_id}
+    track = {"title":song[2], "artist":song[1], "year":year}
     years.append(track)
     print(track)
 
